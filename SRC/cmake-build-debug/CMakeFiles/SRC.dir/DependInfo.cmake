aa83a12e6693618ed1b9aebcee135e473cff1aa6
Sari Saidband <z618149951@gmail.com>
2020-07-05 14:08:48 +0300

    new project in SRC

---
+++ SRC/cmake-build-debug/CMakeFiles/SRC.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/a/Desktop/Sari Seidband/bootcamp 2/C++_week6/dna_analyzer/SRC/main.cpp" "/home/a/Desktop/Sari Seidband/bootcamp 2/C++_week6/dna_analyzer/SRC/cmake-build-debug/CMakeFiles/SRC.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SRC/cmake-build-debug/CMakeFiles/SRC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SRC.dir/main.cpp.o"
+  "SRC"
+  "SRC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SRC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
